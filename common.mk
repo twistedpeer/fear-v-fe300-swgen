9e22ffae37291f971b4ee8ec701de513e02c6d5d
Peer Adelt <peer.adelt@hni.uni-paderborn.de>
2022-03-03 18:58:28 +0100

    riscv-arch-tests: use the same terminator define as all other code generators

--- common.mk
+++ common.mk
@@ -2,6 +2,6 @@ ROOT_DIR := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 BIN_DIR := $(ROOT_DIR)/bin
 SRC_DIR := $(ROOT_DIR)/src
 
-CFLAGS_EXTRA := -march=rv32imac -mabi=ilp32 -DREG_UART_TX=0x10013000
+CFLAGS_EXTRA := -march=rv32imac -mabi=ilp32
 QEMU_MACHINE := sifive_e
 QEMU_TERMINATOR := -device terminator,address=0x80000000
