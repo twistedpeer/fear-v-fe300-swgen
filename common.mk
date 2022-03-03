cf03e15918c052f6f8f39bf8782075bdbc4bd6d3
Peer Adelt <peer.adelt@hni.uni-paderborn.de>
2022-03-03 18:23:46 +0100

    fe300-swgen: all test suites & generators use 0x80000000 for termination

--- common.mk
+++ common.mk
@@ -4,4 +4,4 @@ SRC_DIR := $(ROOT_DIR)/src
 
 CFLAGS_EXTRA := -march=rv32imac -mabi=ilp32 -DREG_UART_TX=0x10013000
 QEMU_MACHINE := sifive_e
-QEMU_TERMINATOR := -device terminator,address=0x80000004
+QEMU_TERMINATOR := -device terminator,address=0x80000000
